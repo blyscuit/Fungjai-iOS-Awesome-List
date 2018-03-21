63fcc6ce66d0d4e339ddc03635de03bfa2bf8bd0
Bliss <blysxaye@icloud.com>
2018-03-21 15:44:59 +0700

    with Siesta and change to model init

--- Fungjai Demo/ViewController.swift
+++ Fungjai Demo/ViewController.swift
@@ -9,11 +9,13 @@
 import UIKit
 import ESPullToRefresh
 import Kingfisher
+import Siesta
 
 class ViewController: UIViewController {
 	@IBOutlet weak var mainTable: UITableView!
 	var tableDataArray: [FungjaiObject]! = []
-	
+	let MyAPI = Service(baseURL: "https://www.anop72.info/api")
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
@@ -21,46 +23,94 @@ class ViewController: UIViewController {
 		self.mainTable.delegate = self
 		self.mainTable.es.addPullToRefresh {
 			[unowned self] in
-			self.loadData()
+//			self.loadData()
+			if self.MyAPI.resource("/seed.json").loadIfNeeded() == nil {
+				self.mainTable.es.stopPullToRefresh()
+			}
 		}
 		
-		loadData()
+//		loadData()
 		
-		// Do any additional setup after loading the view, typically from a nib.
+		MyAPI.resource("/seed.json").addObserver(owner: self) {
+			[weak self] resource, event in
+			guard let _self = self else {
+				DispatchQueue.main.async {
+					self?.mainTable.es.stopPullToRefresh()
+					let alert = UIAlertController(title: "Error", message: "Something went wrong, try again later.", preferredStyle: UIAlertControllerStyle.alert)
+					alert.addAction(UIAlertAction(title: "Close", style: UIAlertActionStyle.default, handler: nil))
+					self?.present(alert, animated: true, completion: nil)
+				}
+				return
+			}
+			if let error = resource.latestError {
+				let alert = UIAlertController(title: "Error", message: error.userMessage, preferredStyle: UIAlertControllerStyle.alert)
+				alert.addAction(UIAlertAction(title: "Close", style: UIAlertActionStyle.default, handler: nil))
+				_self.present(alert, animated: true, completion: nil)
+				_self.mainTable.es.stopPullToRefresh()
+			} else {
+				_self.tableDataArray.removeAll()
+				for obj in resource.jsonArray as! [Dictionary<String, AnyObject>] {
+					let typeDescription = obj["type"] as? String ?? ""
+					switch typeDescription {
+					case "track":
+						_self.tableDataArray.append(FungjaiTrack(dictionary: obj))
+					case "ads":
+						_self.tableDataArray.append(FungjaiAds(dictionary: obj))
+					case "video":
+						_self.tableDataArray.append(FungjaiVideo(dictionary: obj))
+					default:
+						_self.tableDataArray.append(FungjaiVideo(dictionary: obj))
+					}
+				}
+				DispatchQueue.main.async {
+					_self.mainTable.reloadData()
+					_self.mainTable.es.stopPullToRefresh()
+				}
+				UIApplication.shared.isNetworkActivityIndicatorVisible = resource.isLoading
+			}
+		}
+		MyAPI.resource("/seed.json").loadIfNeeded()
+
 	}
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
 	}
 	
+	// URLSession method is not used
+	// To switch to URLSession method, uncomment all loadData() & comment all MyAPI.resource("/seed.json").loadIfNeeded()
 	func loadData() {
-		var url : String = "https://www.anop72.info/api/seed.json"
+		UIApplication.shared.isNetworkActivityIndicatorVisible = true
+		let url : String = "https://www.anop72.info/api/seed.json"
 		
 		URLSession.shared.dataTask(with: URL(string: url)!) { data, response, error in
-			// Handle result
-//			if let response = response {
-//				print(response)
-//			} else {
-//
-//			}
+			UIApplication.shared.isNetworkActivityIndicatorVisible = false
 			do {
 				let json = try JSONSerialization.jsonObject(with: data!) as! [Dictionary<String, AnyObject>]
-				self.tableDataArray = []
+				self.tableDataArray.removeAll()
 				for obj in json {
-					self.tableDataArray.append(FungjaiObject(dictionary: obj))
+					let typeDescription = obj["type"] as? String ?? ""
+					switch typeDescription {
+					case "track":
+						self.tableDataArray.append(FungjaiTrack(dictionary: obj))
+					case "ads":
+						self.tableDataArray.append(FungjaiAds(dictionary: obj))
+					case "video":
+						self.tableDataArray.append(FungjaiVideo(dictionary: obj))
+					default:
+						self.tableDataArray.append(FungjaiVideo(dictionary: obj))
+					}
 				}
-				print(self.tableDataArray)
 				DispatchQueue.main.async {
 					self.mainTable.reloadData()
 					self.mainTable.es.stopPullToRefresh()
 				}
 			} catch {
 				DispatchQueue.main.async {
+					self.mainTable.es.stopPullToRefresh()
 					let alert = UIAlertController(title: "Error", message: "Something went wrong, try again later.", preferredStyle: UIAlertControllerStyle.alert)
 					alert.addAction(UIAlertAction(title: "Close", style: UIAlertActionStyle.default, handler: nil))
 					self.present(alert, animated: true, completion: nil)
-					self.mainTable.es.stopPullToRefresh()
 				}
 			}
 			}.resume()
